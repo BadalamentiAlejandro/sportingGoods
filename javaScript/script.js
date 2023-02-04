aa93ffaa4328a94b2619b9cc120ce3c3aa0e3f67
BadalamentiAlejandro <badalamentialejandro@gmail.com>
2023-02-04 15:39:22 +0100

    agrego carpeta a gitignore y subo nuevo update

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,81 +1,106 @@
 
+const listaDeProductos = [];
+
+
+class productos {
+    constructor(nombre, precio) {
+        this.nombre = nombre;
+        this.precio = parseFloat(precio);
+        listaDeProductos.push(this);
+    }
+    
+}
+
+
+const remera = new productos('remera', 5.53);
+const sweater = new productos('sweater', 12.28);
+const shorts = new productos('shorts', 8.42);
+const zapatillas = new productos('zapatillas', 40.17);
+const pantalones = new productos('pantalones', 15.00);
+const leggings = new productos('leggings', 12.98);
+const botines = new productos('botines', 50.00);
+
+
+const listaDeCupones = ['10', '25', '30', '50']
+
+
 let cesta = 0;
 let precioTotal = 0;
 
 const agregarAlCarro = () => {
     alert('Bienvenido a tu tienda de ropa deportiva favorita. Nuestros productos especiales de hoy son: Remera, Sweater, Shorts, Zapatillas, Pantalones, Leggings y Botines');
 
-    let productoElegido = prompt('Escribe un producto, por favor! Remera, Sweater, Shorts, Zapatillas, Pantalones, Leggings, Botines o escribe Finalizar para salir').toLocaleLowerCase();
+    let productoElegido = prompt('Deseas agregar un producto a tu carrito de compras? Escribe alguna de las opciones: Remera, Sweater, Shorts, Zapatillas, Pantalones, Leggings, Botines o escribe "no" para salir').toLowerCase();
 
-    while (productoElegido != 'finalizar') {
+    while (productoElegido != 'no') {
         switch (productoElegido) {
-            case 'remera':
-                cesta = 5;
-                alert('Haz elegido ' + productoElegido + ' y su precio es de ' + cesta + ' Dolares');
+            case listaDeProductos[0].nombre:
+                cesta = listaDeProductos[0].precio;
+                alert('Haz elegido ' + listaDeProductos[0].nombre + ' y su precio es de ' + (listaDeProductos[0].precio).toFixed(2) + ' Dolares');
                 break;
-            case 'sweater':
-                cesta = 12;
-                alert('Haz elegido ' + productoElegido + ' y su precio es de ' + cesta + ' Dolares');
+            case listaDeProductos[1].nombre:
+                cesta = listaDeProductos[1].precio;
+                alert('Haz elegido ' + listaDeProductos[1].nombre + ' y su precio es de ' + (listaDeProductos[1].precio).toFixed(2) + ' Dolares');
                 break;
-            case 'shorts':
-                cesta = 8;
-                alert('Haz elegido ' + productoElegido + ' y su precio es de ' + cesta + ' Dolares');
+            case listaDeProductos[2].nombre:
+                cesta = listaDeProductos[2].precio;
+                alert('Haz elegido ' + listaDeProductos[2].nombre + ' y su precio es de ' + (listaDeProductos[2].precio).toFixed(2) + ' Dolares');
                 break;
-            case 'zapatillas':
-                cesta = 40;
-                alert('Haz elegido ' + productoElegido + ' y su precio es de ' + cesta + ' Dolares');
+            case listaDeProductos[3].nombre:
+                cesta = listaDeProductos[3].precio;
+                alert('Haz elegido ' + listaDeProductos[3].nombre + ' y su precio es de ' + (listaDeProductos[3].precio).toFixed(2) + ' Dolares');
                 break;
-            case 'pantalones':
-                cesta = 15;
-                alert('Haz elegido ' + productoElegido + ' y su precio es de ' + cesta + ' Dolares');
+            case listaDeProductos[4].nombre:
+                cesta = listaDeProductos[4].precio;
+                alert('Haz elegido ' + listaDeProductos[4].nombre + ' y su precio es de ' + (listaDeProductos[4].precio).toFixed(2) + ' Dolares');
                 break;
-            case 'leggings':
-                cesta = 12;
-                alert('Haz elegido ' + productoElegido + ' y su precio es de ' + cesta + ' Dolares');
+            case listaDeProductos[5].nombre:
+                cesta = listaDeProductos[5].precio;
+                alert('Haz elegido ' + listaDeProductos[5].nombre + ' y su precio es de ' + (listaDeProductos[5].precio).toFixed(2) + ' Dolares');
                 break;
-            case 'botines':
-                cesta = 50;
-                alert('Haz elegido ' + productoElegido + ' y su precio es de ' + cesta + ' Dolares');
+            case listaDeProductos[6].nombre:
+                cesta = listaDeProductos[6].precio;
+                alert('Haz elegido ' + listaDeProductos[6].nombre + ' y su precio es de ' + (listaDeProductos[6].precio).toFixed(2) + ' Dolares');
                 break;
             default: cesta
                 cesta = 0;
                 alert('Introduce un producto correcto');
         }
-        precioTotal = precioTotal + cesta
-        productoElegido = prompt('Escribe otro producto, por favor! Remera, Sweater, Shorts, Zapatillas, Pantalones, Leggings, Botines o escribe Finalizar para salir').toLocaleLowerCase();
+        precioTotal = precioTotal + cesta;
+        productoElegido = prompt('Escribe otro producto, por favor! Remera, Sweater, Shorts, Zapatillas, Pantalones, Leggings, Botines o escribe "no" para salir').toLowerCase();
     }
 }
 
 agregarAlCarro();
 
-console.log(precioTotal);
-alert('Su carro tiene productos por un total de ' + precioTotal + ' dolares');
+console.log(precioTotal.toFixed(2));
+alert('Su carro tiene productos por un total de ' + precioTotal.toFixed(2) + ' dolares');
 
 const activarCuponDeDescuento = () => {
     console.log('Los números de cupon son 10, 25, 30 o 50');
-    let numeroDeCupon = prompt('Escribe el número de cupon de descuento si posees uno, sino escribe Finalizar (Los números de cupon son 10, 25, 30 o 50)').toLocaleLowerCase();
+    let numeroDeCupon = prompt('Escribe el número de cupon de descuento si posees uno, sino escribe "no" (Los números de cupon son 10, 25, 30 o 50)').toLowerCase();
 
-    while (numeroDeCupon != 'finalizar') {
+    while (numeroDeCupon != 'no') {
         switch (numeroDeCupon) {
-            case '10':
-                precioTotal = precioTotal - (precioTotal * 0.10);
+            case listaDeCupones[0]:
+                precioTotal = (precioTotal - (precioTotal * 0.10)).toFixed(2);
                 break;
-            case '25':
-                precioTotal = precioTotal - (precioTotal * 0.25);
+            case listaDeCupones[1]:
+                precioTotal = (precioTotal - (precioTotal * 0.25)).toFixed(2);
                 break;
-            case '30':
-                precioTotal = precioTotal - (precioTotal * 0.30);
+            case listaDeCupones[2]:
+                precioTotal = (precioTotal - (precioTotal * 0.30)).toFixed(2);
                 break;
-            case '50':
-                precioTotal = precioTotal - (precioTotal * 0.50);
+            case listaDeCupones[3]:
+                precioTotal = (precioTotal - (precioTotal * 0.50)).toFixed(2);
                 break;
             default:
                 alert('El número introducido es incorrecto, vuelve a intentar');
         }
-        numeroDeCupon = prompt('Escribe un número de cupon correcto de descuento si posees uno, sino escribe Finalizar');
+        numeroDeCupon = prompt('Escribe un número de cupon correcto de descuento si posees uno, sino escribe "no"');
     }
     console.log(precioTotal);
-    alert('El total de tu compra tiene un precio de ' + parseFloat(precioTotal));
+    alert('El total de tu compra tiene un precio de ' + precioTotal + ' dolares');
 }
 
 activarCuponDeDescuento();
