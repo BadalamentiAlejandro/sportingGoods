4fd3987e3964ce88e41f85e440b59addbbdc86d9
BadalamentiAlejandro <badalamentialejandro@gmail.com>
2023-03-31 21:46:55 +0200

    commit final, ultima entrega

--- javaScript/scriptMiCesta.js
+++ javaScript/scriptMiCesta.js
@@ -1,104 +1,121 @@
 
 //Array para la cesta
-const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
-
-
-//Manipulamos el DOM de miCesta.
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 const containerProducto = document.getElementById("containerProducto");
+const containerTotal = document.getElementById("containerTotal");
 
+//Función por la cual renderizo el carrito en el modal que se despliega con el widget de carrito.
+function verCarrito() {
+    if (carrito.length === 0) {
+        containerProducto.innerHTML = `
+        <h5>Tu carrito está vacío</h5>
+        `
+    } else {
+        containerProducto.innerHTML = "";
+        carrito.forEach(e => {
+            const { id, imagen, nombre, precio, cantidad } = e;
 
-//Este loop imprime los productos y los precios de la cesta.
-function imprimirContainer() {
-    for (const producto of carrito) {
-        containerProducto.innerHTML += `
-                        <li class="lista">
-                            <img class="imagen" src= "${producto.imagen}">
-                            <p class="nombre">${producto.nombre}</p>
-                            <p class="precio">$${producto.precio}</p>
-                            <div class="containerContador">
-                                <button class="botonEliminar"> Quitar </button>
-                                <p class="textoContador" id="textoContador"> 1 </p>
-                                <button class="botonSumar"> Sumar </button>
-                            </div>
-                        </li>    
-                            `;
-    }
-};
-imprimirContainer()
+            containerProducto.innerHTML += `
+            <div class="contenedorModal"> 
+                <div>
+                    <img class="imagen" src="${imagen}">
+                </div>
 
+                <div>
+                    <p class="nombre">Nombre: ${nombre}</p>
+                    <p class="precio">Precio: $${precio}</p>
+                    <p class="cantidad">Cantidad: ${cantidad}</p>
 
-const textoContador = document.getElementsByClassName("textoContador");
-const botonSumar = document.getElementsByClassName("botonSumar");
-//Función para sumar productos.
-function sumarElemento(botonSumar) {
-    let count = 1
-    for (let i = 0; i < botonSumar.length; i++) {
-        botonSumar[i].addEventListener("click", () => {
-            count++;
-            textoContador[i].innerHTML = count;
+                    <button class="botonAgregar" onclick="funcinoRestar(${id})">-</button>
+                    <button class="botonAgregar" onclick="funcionEliminar(${id})">Eliminar Producto</button>
+                    <button class="botonAgregar" onclick="funcionAgregar(${id})">+</button>
+                </div>
+            </div>
+            `
         })
-    
     }
-}
-sumarElemento(botonSumar);
+    //Metodo reduce para poder dar con la suma total de todos los productos.
+    containerTotal.textContent = "Precio toal: " + carrito.reduce((acc, prod) => acc + prod.cantidad * prod.precio, 0).toFixed(2);
+};
 
+//Función por la cual se puede agregar un producto al carrito.
+function funcionAgregar(id) {
+    //Primero nos fijamos si el producto existe con el método "some()" ya que nos devolvera true o false.
+    const productoExiste = carrito.some((e) => parseFloat(e.id) === id);
 
-const botonEliminar = document.getElementsByClassName("botonEliminar");
-//Funcion para eliminar elemento del STORAGE.
-function eliminarElemento(botonEliminar) {
-    for (let i = 0; i < botonEliminar.length; i++) {
-        botonEliminar[i].addEventListener("click", () => {
-            carrito.splice(i, 1);
-            localStorage.clear();
+    //Si nos devuelve true.
+    if (productoExiste) {
+        //Aquí mapeamos 
+        const productoEncontrado = carrito.map((e) => {
+            if (parseFloat(e.id) === id) {
+                e.cantidad++
+                localStorage.setItem("carrito", JSON.stringify(carrito));
+                swal({
+                    title: "Producto agregado correctamente!",
+                    icon: "success",
+                })
+            }
+        })
+    }
+    verCarrito();
+};
+
+//Función para restar y eliminar productos del carrito.
+function funcinoRestar(id) {
+    //Buscamos que el producto exista.
+    const productoExiste = carrito.some((e) => parseFloat(e.id) === id);
+
+    if (productoExiste) {
+        //Aquí buscamos el producto en el carrito.
+        const productoEncontrado = carrito.find((e) => parseFloat(e.id) === id)
+
+        //Si la cantidad es mayor a 1 entonces resta la cantidad del producto en uno. De lo contrario se remite a la funcionEliminar.
+        if (productoEncontrado.cantidad > 1) {
+            productoEncontrado.cantidad--
             localStorage.setItem("carrito", JSON.stringify(carrito));
-            location.reload();//Se que esto no es lo mejor, pero es lo que encontré para poder refrescar la funcion imprimirContainer.
+            swal({
+                title: "Producto eliminado correctamente!",
+                icon: "success",
+            })
+        } else {
+            funcionEliminar(id);
+        }
+    }
+    verCarrito();
+
+}
+
+//Función por la cual podemos eliminar productos del carrito.
+function funcionEliminar(id) {
+    carrito = carrito.filter(prod => parseFloat(prod.id) !== id);
+    swal({
+        title: "Producto eliminado correctamente!",
+        icon: "success",
+    })
+    localStorage.clear();
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    verCarrito();
+};
+
+//Función que utilizamos para vaciar el carrito de forma completa.
+function vaciarCarrito() {
+    carrito = [];
+    localStorage.clear();
+    verCarrito();
+};
+
+function realizarCompra() {
+    if (carrito.length === 0) {
+        swal({
+            title: "Dirígete a la seccion ROPA para seleccionar un producto!",
+            icon: "info",
+        })
+    } else {
+        swal({
+            title: "Compra realizada con exito!",
+            icon: "success",
         })
     }
 };
-eliminarElemento(botonEliminar);
 
-
-let total = 0;
-//Funcion que suma el precio total de todos los items en el carrito.
-function sumarTotal() {
-    for (let i = 0; i < carrito.length; i++) {
-
-        total += carrito[i].precio;
-    };
-};
-sumarTotal();
-
-
-
-let containerTotal = document.getElementById("containerTotal");
-//Este elemento imprime el precio total de la compra.
-containerTotal.innerHTML = `
-                            <h2>Precio Total: $${total.toFixed(2)} </h2>
-                            <button id="botonComprar" class="botonAgregar">Comprar</button>
-                            `;
-const botonComprar = document.getElementById("botonComprar");
-function comprar() {
-    botonComprar.addEventListener("click", () => {
-        if (!carrito.length) {
-            swal({
-                title: "Lo sentimos, no tienes ningun producto en la cesta. Agrega un producto en la sección Ropa para poder realizar la compra",
-                icon: "error",
-            });
-        } else {
-            swal({
-                title: "Haz realizado la compra correctamente por un total de",
-                text: `$${total.toFixed(2)}`,
-                icon: "success",
-            });
-        };
-
-    });
-}
-comprar();
-
-
-
-
-
-
-console.log(carrito);
+verCarrito();
--- javaScript/scriptRopa.js
+++ javaScript/scriptRopa.js
@@ -1,79 +1,142 @@
 
-// //lista de productos pre creada con un constructor, esto no se usa mas ya que se hace fetch desde un .json.
-// const listaDeProductos = [];
-// class productos {
-//     constructor(nombre, precio) {
-//         this.nombre = nombre;
-//         this.precio = parseFloat(precio);
-//         listaDeProductos.push(this);
-//     }
-
-// }
-// const remera = new productos('remera', '5.53');
-// const sweater = new productos('sweater', '12.28');
-// const shorts = new productos('shorts', '8.42');
-// const zapatillas = new productos('zapatillas', '40.17');
-// const pantalones = new productos('pantalones', '15.00');
-// const leggings = new productos('leggings', '12.98');
-// const botines = new productos('botines', '50.00');
-
-
-//Array para la cesta.
-const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
-
-//Container principal donde se imprime las tarjetas de cada producto.
+const listaProductos = [];
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 const container = document.getElementById("container");
+const precioTotal = document.getElementById("precioTotal");
 
-//Función asincronica para traer los productos de un archivo .json
+
+//Utilizo una función asincrónica para poder hacer un fetch del documento json con los datos de los productos.
 async function productos() {
     const data = await fetch("../index.json");
     const prods = await data.json();
-    
-    //Loop para poder imprimir en pantalla los productos.
-    prods.store.forEach(element => {
+
+    //Mediante una función de desestructuración paso la información fuera de la asincronía para poder utilizarla en otras funciones.
+    listaProductos.push(...prods.store);
+
+    prods.store.forEach(e => {
+        //Si bien no es necesario desestructurar aquí, lo utilizo para crear un codigo mas limpio.
+        const { id, imagen, nombre, precio } = e;
+
         container.innerHTML += `
         <li class="lista">
-            <img class="imagen" src= "${element.imagen}">
-            <p class="nombre">${element.nombre}</p>
-            <p class="precio">$${element.precio}</p>
-            <button class="botonAgregar">AGREGAR A MI CESTA</button>
+        <img class="imagen" src= "${imagen}">
+        <p class="nombre">${nombre}</p>
+        <p class="precio">Precio: $${precio}</p>
+        
+        <button class="botonAgregar" onclick="funcionAgregar(${id})">AGREGAR A MI CESTA</button>
         </li>
         `
     });
+};
 
-    const botonAgregar = document.getElementsByClassName("botonAgregar");
-    //Función gracias a la cual agregamos productos al carrito, de ahi al storage y un sweet alert para confirmar el movimiento.
-    function funcionBoton(el) {
-        for (let i = 0; i < el.length; i++) {
-            el[i].addEventListener("click", () => {
+//Función por la cual se puede agregar un producto al carrito.
+async function funcionAgregar(id) {
+    //Primero nos fijamos si el producto existe con el método "some()" ya que nos devolvera true o false.
+    const productoExiste = carrito.some((e) => parseFloat(e.id) === id);
 
-                if(!carrito[i]) {
-
-                    carrito.push(prods.store[i]);
-                    localStorage.setItem("carrito", JSON.stringify(carrito));                    
-                    swal({
-                        title: "Producto agregado correctamente!",
-                        text: `${prods.store[i].nombre}
-                               $${prods.store[i].precio}`,
-                        icon: "success",
-                    });
-                } else {
-                    swal({
-                        title: "Ya tienes este producto en tu cesta",
-                        text: "Para agregar mas o quitarlo, dirígete a la sección Mi Cesta",
-                        icon: "warning",
-                    });
-                }
-
-                } 
-            )}
-        }
-        funcionBoton(botonAgregar);
+    //Si nos devuelve true.
+    if (productoExiste) {
+        //Aquí mapeamos 
+        const productoEncontrado = carrito.map((e) => {
+            if (parseFloat(e.id) === id) {
+                e.cantidad++
+                localStorage.setItem("carrito", JSON.stringify(carrito));
+                swal({
+                    title: "Producto agregado correctamente!",
+                    icon: "success",
+                })
+            }
+        })
+    } else {
+        //En este caso no puedo usar el metodo "some()" porque me retorna un valor booleano y no el valor que necesito, por lo cual no imprime en pantalla. En cambio utilizando el metodo "find()", me devuelve el valor solicitado.
+        const item = listaProductos.find((e) => parseFloat(e.id) === id);
+        carrito.push(item);
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+        swal({
+            title: "Producto agregado correctamente!",
+            icon: "success",
+        })
     }
+    verCarrito();
+};
 
+// //Función para restar y eliminar productos del carrito.
+// function funcinoRestar(id) {
+//     //Buscamos que el producto exista.
+//     const productoExiste = carrito.some((e) => parseFloat(e.id) === id);
 
+//     if (productoExiste) {
+//         //Aquí buscamos el producto en el carrito.
+//         const productoEncontrado = carrito.find((e) => parseFloat(e.id) === id)
 
+//         //Si la cantidad es mayor a 1 entonces resta la cantidad del producto en uno. De lo contrario se remite a la funcionEliminar.
+//         if (productoEncontrado.cantidad > 1) {
+//             productoEncontrado.cantidad--
+//             localStorage.setItem("carrito", JSON.stringify(carrito));
+//             swal({
+//                 title: "Producto eliminado correctamente!",
+//                 icon: "success",
+//             })
+//         } else {
+//             funcionEliminar(id);
+//         }
+//     }
+//     verCarrito();
 
+// }
 
+// //Función por la cual renderizo el carrito en el modal que se despliega con el widget de carrito.
+// function verCarrito() {
+//     if (carrito.length === 0) {
+//         modalBody.innerHTML = `
+//         <h5>Tu carrito está vacío</h5>
+//         `
+//     } else {
+//         modalBody.innerHTML = "";
+//         carrito.forEach(e => {
+//             const { id, imagen, nombre, precio, cantidad } = e;
+
+//             modalBody.innerHTML += `
+//             <div class="contenedorModal"> 
+//                 <div>
+//                     <img class="imgCarrito" src="${imagen}">
+//                 </div>
+
+//                 <div>
+//                     <p>Nombre: ${nombre}</p>
+//                     <p>Precio: $${precio}</p>
+//                     <p>Cantidad: ${cantidad}</p>
+
+//                     <button class="botonEliminar" onclick="funcinoRestar(${id})">-</button>
+//                     <button class="botonEliminar" onclick="funcionEliminar(${id})">Eliminar Producto</button>
+//                     <button class="botonEliminar" onclick="funcionAgregar(${id})">+</button>
+//                 </div>
+//             </div>
+//             `
+//         })
+//     }
+//     //Metodo reduce para poder dar con la suma total de todos los productos.
+//     precioTotal.textContent = "Precio toal: " + carrito.reduce((acc, prod) => acc + prod.cantidad * prod.precio, 0).toFixed(2);
+// };
+
+// //Función por la cual podemos eliminar productos del carrito.
+// function funcionEliminar(id) {
+//     carrito = carrito.filter(prod => parseFloat(prod.id) !== id);
+//     swal({
+//         title: "Producto eliminado correctamente!",
+//         icon: "success",
+//     })
+//     localStorage.clear();
+//     localStorage.setItem("carrito", JSON.stringify(carrito));
+//     verCarrito();
+// };
+
+// //Función que utilizamos para vaciar el carrito de forma completa.
+// function vaciarCarrito() {
+//     carrito = [];
+//     localStorage.clear();
+//     verCarrito();
+// };
+
+// verCarrito();
 productos();
-
